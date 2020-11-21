3e4ef55425b3b986d730b7fafaefe7c48b65a2df
Turki <abosaad.us@gmail.com>
2020-11-22 02:23:44 +0300

    </>

---
+++ Django/books_authors/books_authors/secret_settings.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '(hs%(cb723o(7+=td7gvgp#m-3=#*k*1@_=2ee186d=e6w(7dk'
--- Django/books_authors/books_authors/settings.py
+++ Django/books_authors/books_authors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from . import secret_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(hs%(cb723o(7+=td7gvgp#m-3=#*k*1@_=2ee186d=e6w(7dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
